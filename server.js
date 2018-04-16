bba071d5c2d82b3cbe3dfb5126fdc4e8849e3211
Bayron Villanueva <bvillanueva@cognitiva.la>
2018-04-15 21:59:56 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    console.log(log);
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         pageTitle: 'We will be right back soon',
+//         welcomeMessage: 'We are currently working in this page, be patient :)'
+//     })
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+
+    // res.send('<h1>Hello Express!</h1>')
+    res.render('home.hbs', {
+        pageTitle: 'Spectacular Website',
+        welcomeMessage: 'Welcome to this spectacular website'
+    })
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+
+    res.send({
+        errorMessage: 'Oops, some error has occurred while handling your request. Try again later please'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+});
